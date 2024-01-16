62d85f3010ce2190ae5dda66b721ef956acb24b3
omargonz01 <gomarzalez@gmail.com>
2024-01-16 12:17:23 -0800

    edit game over function

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const playAgainButton = document.querySelector('.play-again')
 
 const totalCells = 100;
 const totalBombs = 90;
-const maxScore = 5;
+const maxScore = 15;
 const bombsList = [];
 
 let score = 0;
@@ -18,22 +18,36 @@ let score = 0;
 function updateScore() {
   score++;
   scoreCounter.innerText = score.toString().padStart(5, '0');
+
+  if (score === maxScore) {
+    endGame(true);
+  }
 }
 
-for (let i = 1; i <=100; i++ ) {
+const cell = document.createElement('div');
+cell.classList.add('cell');
+//<div class="cell"></div>
+
+for (let i = 1; i <= 100; i++) {
+  //cell = <div></div>
   const cell = document.createElement('div');
   cell.classList.add('cell');
+  //<div class="cell"></div>
 
   cell.addEventListener('click', function () {
-
+    updateScore();
+    if (bombsList.includes(i)) {
+      cell.classList.add('cell-bomb');
+      endGame(false);
+    }
     cell.classList.add('cell-clicked');
-  })
+  });
   grid.appendChild(cell);
 }
 
 while (bombsList.length < totalBombs) {
   //  Generate a random number between 1 & 100,inclusive
-  const randomNumber= Math.floor(Math.random() * totalCells) + 1;
+  const randomNumber = Math.floor(Math.random() * totalCells) + 1;
   
   if (!bombsList.includes(randomNumber)) {
     bombsList.push(randomNumber);
@@ -44,6 +58,10 @@ function endGame(isVictory) {
   if (isVictory) {
     endGameText.innerHTML = 'YOU<br>WON';
     endGameScreen.classList.add('win');
+  } else {
+    endGameText.innerHTML = 'YOU<br>LOST';
+    endGameScreen.classList.add('lost');
+    gameOver();
   }
   endGameScreen.classList.remove('hidden');
 }
@@ -51,3 +69,13 @@ function endGame(isVictory) {
 playAgainButton.addEventListener('click', function () {
   window.location.reload();
 });
+
+const cells= document.getElementsByClassName('cell');
+
+function gameOver(){
+  for(let j=1; j<=100; j++)
+  {
+    if(bombsList.includes(j))
+    cells[j].classList.add('cell-bomb');
+  }
+}
